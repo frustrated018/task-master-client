a04ab01df48e78c7fcebdeade1314db9e0e391da
frustrated018 <niloyd018@gmail.com>
2023-12-22 19:31:04 +0600

    initial setup done with react-router-dom, tailwind CSS & daisyUI

---
+++ src/Pages/Home/homePage.jsx
@@ -0,0 +1,10 @@
+
+const homePage = () => {
+    return (
+        <div>
+            <h2 className="text-5xl text-center">This is the home page</h2>
+        </div>
+    );
+};
+
+export default homePage;
