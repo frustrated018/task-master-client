ccbc44222757f38ce7d6fdd6a60c041f789b9c5b
frustrated018 <niloyd018@gmail.com>
2023-12-22 19:46:16 +0600

    Added a user image

--- src/Pages/Home/homePage.jsx
+++ src/Pages/Home/homePage.jsx
@@ -1,10 +1,11 @@
+import HomeNavBar from "../../Components/Navbar/HomeNavBar";
 
 const homePage = () => {
-    return (
-        <div>
-            <h2 className="text-5xl text-center">This is the home page</h2>
-        </div>
-    );
+  return (
+    <>
+      <HomeNavBar />
+    </>
+  );
 };
 
 export default homePage;
